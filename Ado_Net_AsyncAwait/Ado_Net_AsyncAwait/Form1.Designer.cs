fc9260318b896ce271ae95510103a2451cc5e721
Anatoliy <toxerius7@gmail.com>
2018-03-26 11:00:06 +0300

    Finished Project

--- Ado_Net_AsyncAwait/Ado_Net_AsyncAwait/Form1.Designer.cs
+++ Ado_Net_AsyncAwait/Ado_Net_AsyncAwait/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 154);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read From File";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(538, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Query To DataBase";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(486, 276);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 426);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Ado_Net_AsyncAwait/Ado_Net_AsyncAwait/Form1.cs
+++ Ado_Net_AsyncAwait/Ado_Net_AsyncAwait/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,74 @@ namespace Ado_Net_AsyncAwait
 {
     public partial class Form1 : Form
     {
+        string connectionString = @"Data Source=CR5-00\SQLEXPRESS;Initial Catalog=Library; Integrated Security=true;";
+        SqlConnection sqlConnection = null;
+        DataTable dataTable = null;
         public Form1()
         {
             InitializeComponent();
+            button1.Click += Button1_Click;
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Before async call");
+                await GetDataAsync(open.FileName);
+                MessageBox.Show("After asyc call");
+                
+            }
+        }
+
+        async private Task GetDataAsync(string fileName)
+        {
+            byte[] data = null;
+            using (FileStream fs = File.Open(fileName, FileMode.Open))
+            {
+                data = new byte[fs.Length];
+                await fs.ReadAsync(data, 0, (int)fs.Length);
+            }
+            textBox1.Text = Encoding.Default.GetString(data);
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            using(sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                string sql = "waitfor delay '00:00:10'";
+                sql += textBox2.Text;
+                SqlCommand sqlCommand = new SqlCommand(sql,sqlConnection);
+                dataTable = new DataTable();
+                SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+                int line = 0;
+                do
+                {
+                    while (await sqlDataReader.ReadAsync())
+                    {
+                        if(line++ == 0)
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(sqlDataReader.GetName(i));
+                            }
+                        }
+
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        {
+                            dataRow[i] = await sqlDataReader.GetFieldValueAsync<Object>(i);
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                } while (await sqlDataReader.NextResultAsync());
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dataTable;
+            }
         }
     }
 }
